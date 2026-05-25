2d97a0d394872c9f9b6f0c679c15ff813d6e5a5b
Manish Kumar <manish.kumar@basf.com>
2026-05-26 00:57:25 +0200

    refactor(configs)!: update __init__ to import from core, models/, and experimental/

--- deeptab/configs/__init__.py
+++ deeptab/configs/__init__.py
@@ -1,25 +1,22 @@
-from .autoint_config import AutoIntConfig
-from .base_config import BaseConfig
-from .base_model_config import BaseModelConfig
-from .enode_config import ENODEConfig
-from .fttransformer_config import FTTransformerConfig
-from .mambatab_config import MambaTabConfig
-from .mambattention_config import MambAttentionConfig
-from .mambular_config import MambularConfig
-from .mlp_config import MLPConfig
-from .modernnca_config import ModernNCAConfig
-from .ndtf_config import NDTFConfig
-from .node_config import NODEConfig
-from .preprocessing_config import PreprocessingConfig
-from .resnet_config import ResNetConfig
-from .saint_config import SAINTConfig
-from .tabm_config import TabMConfig
-from .tabr_config import TabRConfig
-from .tabtransformer_config import TabTransformerConfig
-from .tabularnn_config import TabulaRNNConfig
-from .tangos_config import TangosConfig
-from .trainer_config import TrainerConfig
-from .trompt_config import TromptConfig
+from .core import BaseConfig, BaseModelConfig, PreprocessingConfig, TrainerConfig
+from .experimental.modernnca_config import ModernNCAConfig
+from .experimental.tangos_config import TangosConfig
+from .experimental.trompt_config import TromptConfig
+from .models.autoint_config import AutoIntConfig
+from .models.enode_config import ENODEConfig
+from .models.fttransformer_config import FTTransformerConfig
+from .models.mambatab_config import MambaTabConfig
+from .models.mambattention_config import MambAttentionConfig
+from .models.mambular_config import MambularConfig
+from .models.mlp_config import MLPConfig
+from .models.ndtf_config import NDTFConfig
+from .models.node_config import NODEConfig
+from .models.resnet_config import ResNetConfig
+from .models.saint_config import SAINTConfig
+from .models.tabm_config import TabMConfig
+from .models.tabr_config import TabRConfig
+from .models.tabtransformer_config import TabTransformerConfig
+from .models.tabularnn_config import TabulaRNNConfig
 
 __all__ = [
     "AutoIntConfig",
