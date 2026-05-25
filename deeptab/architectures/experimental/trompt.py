35364cbf793c216e7dd57483f03c024a360f46fb
Manish Kumar <manish.kumar@basf.com>
2026-05-26 00:25:47 +0200

    feat(architectures)!: add experimental sub-package with ModernNCA, Tangos, Trompt

---
+++ deeptab/architectures/experimental/trompt.py
@@ -0,0 +1,54 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+from deeptab.configs.trompt_config import TromptConfig
+from deeptab.core.base_model import BaseModel
+from deeptab.nn.blocks.common import EmbeddingLayer
+from deeptab.nn.blocks.trompt import TromptCell, TromptDecoder
+from deeptab.nn.normalization import get_normalization_layer
+
+
+class Trompt(BaseModel):
+    def __init__(
+        self,
+        feature_information: tuple,  # Expecting (num_feature_info, cat_feature_info, embedding_feature_info)
+        num_classes=1,
+        config: TromptConfig = TromptConfig(),  # noqa: B008
+        **kwargs,
+    ):
+        super().__init__(config=config, **kwargs)
+        self.save_hyperparameters(ignore=["feature_information"])
+        self.returns_ensemble = True
+
+        # embedding layer
+        self.cells = nn.ModuleList(TromptCell(feature_information, config) for _ in range(config.n_cycles))
+        self.decoder = TromptDecoder(config.d_model, num_classes)
+        self.init_rec = nn.Parameter(torch.empty(config.P, config.d_model))
+        self.n_cycles = config.n_cycles
+
+    def forward(self, *data):
+        """Defines the forward pass of the model.
+
+        Parameters
+        ----------
+        data : tuple
+            Input tuple of tensors of num_features, cat_features, embeddings.
+
+        Returns
+        -------
+        Tensor
+            The output predictions of the model.
+        """
+        O = self.init_rec.unsqueeze(0).repeat(data[0][0].shape[0], 1, 1)  # noqa: E741
+        outputs = []
+
+        for i in range(self.n_cycles):
+            O = self.cells[i](*data, O=O)  # noqa: E741
+            # print(O.shape)
+            # print(self.tdown(O).shape)
+            outputs.append(self.decoder(O))
+
+        out = torch.stack(outputs, dim=1).squeeze(-1)
+        # preds = out.mean(dim=1)
+        return out
