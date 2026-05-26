bfadf73776d1f82230ef8c4f3bbf41351dd377ea
Manish Kumar <manish.kumar@basf.com>
2026-05-26 15:25:02 +0200

    refactor(hpo): add missing exports to hpo/__init__.py

--- deeptab/hpo/__init__.py
+++ deeptab/hpo/__init__.py
@@ -1,5 +1,7 @@
-from .mapper import get_search_space
+from .mapper import activation_mapper, get_search_space, round_to_nearest_16
 
 __all__ = [
+    "activation_mapper",
     "get_search_space",
+    "round_to_nearest_16",
 ]
