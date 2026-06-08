05b464e372c77cb316d587d7ed0573ce498e3d0f
Manish Kumar <manish.kumar@basf.com>
2026-06-08 22:14:00 +0200

    feat(api): export exception and warning types from deeptab and deeptab.core

--- deeptab/__init__.py
+++ deeptab/__init__.py
@@ -1,10 +1,24 @@
 from . import configs, data, distributions, metrics, models
 from ._version import __version__
+from .core.exceptions import (
+    ConfigWarning,
+    DataWarning,
+    DeepTabError,
+    DeepTabWarning,
+    NotFittedError,
+    PerformanceWarning,
+)
 from .core.inference import InferenceModel
 from .core.reproducibility import seed_context, set_seed
 
 __all__ = [
+    "ConfigWarning",
+    "DataWarning",
+    "DeepTabError",
+    "DeepTabWarning",
     "InferenceModel",
+    "NotFittedError",
+    "PerformanceWarning",
     "__version__",
     "configs",
     "data",
